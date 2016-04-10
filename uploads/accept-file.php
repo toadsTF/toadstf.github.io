49c8dc24fd917e13d07a23497f49d500f596f703
toads <bridges.anson@gmail.com>
2016-04-09 20:37:37 -0700

    updated site. lewisclark will return

--- uploads/accept-file.php
+++ uploads/accept-file.php
@@ -3,23 +3,18 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-// Check if image file is a actual image or fake image
-if(isset($_POST["submit"])) {
-    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    if($check !== false) {
-        echo "File is an image - " . $check["mime"] . ".";
-        $uploadOk = 1;
-    } else {
-        echo "File is not an image.";
-        $uploadOk = 0;
-    }
-}
 // Check if file already exists
 if (file_exists($target_file)) {
     echo "Sorry, file already exists.";
     $uploadOk = 0;
 }
+// Allow certain file formats
+if($imageFileType != "ezm") {
+    echo "Sorry, only.EZM files are allowed.";
+    $uploadOk = 0;
+}
 // Check file size
+
 if ($_FILES["fileToUpload"]["size"] > 500000) {
     echo "Sorry, your file is too large.";
     $uploadOk = 0;
@@ -30,7 +25,7 @@ if ($uploadOk == 0) {
 // if everything is ok, try to upload file
 } else {
     if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.",  PHP_EOL, "\r\ncheck out the list of files <a href=\"../uploads/uploads">here.</a>";
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.\r\n check out the list of files <a href=\"uploads/\">here.</a>";
     } 
 	else {
         echo "Sorry, there was an error uploading your file.";
